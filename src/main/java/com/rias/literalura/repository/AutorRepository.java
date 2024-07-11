c3f20eac26ebd032de847123514730aafe0f83a4
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-07-10 21:55:31 -0600

    Init en 10/07/2024

--- src/main/java/com/rias/literalura/repository/AutorRepository.java
+++ src/main/java/com/rias/literalura/repository/AutorRepository.java
@@ -2,7 +2,6 @@ package com.rias.literalura.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rias.literalura.modelos.Autor;
-import java.util.List;
 
 
 public interface AutorRepository extends JpaRepository<Autor,Long>{
